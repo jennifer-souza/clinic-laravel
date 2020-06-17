c149c3b9ae0c699afad2e725d4bb37500ca41454
jenvieiras <jennifervsouza@icloud.com>
2020-06-16 22:32:21 -0300

    show columns when screen large

--- multi/resources/views/listProfessional.blade.php
+++ multi/resources/views/listProfessional.blade.php
@@ -3,48 +3,54 @@
 @include('head')
 	<body>
     @include('header')
-		<div class="prof">
-			<h2 class="border border-secondary rounded bg-secondary text-white col-md-12">Lista de Profissionais</h2>
+		<div class="container">
+			<h2 class="border border-primary rounded bg-primary text-white col-md-12">Lista de Profissionais</h2>
 			<hr />
-			<table class="table table-hover table-sm border border-secondary">
-			    <tr>
-			      	<td class="table-dark">Nome</td>
-			      	<td class="table-dark">Área de Atuação</td>
-			      	<td class="table-dark">Licença Profissional</td>
-			      	<td class="table-dark">CPF</td>
-			      	<td class="table-dark">Celular</td>
-					<td colspan="3" class="table-dark">
-						<a href="{{route('professional.create')}}" style="padding-left: 50%;">
-							<button class="btn btn-primary btn-sm">Novo profissional</button>
-						</a>
-					</td>
-			    </tr>
-				@foreach($professionals as $professional)
-				<tr>
-					<td class="table-active">{{$professional->name}}</td>
-					<td class="table-active">{{$professional->prof}}</td>
-					<td class="table-active">{{$professional->license}}</td>
-					<td class="table-active">{{$professional->cpf}}</td>
-					<td class="table-active">{{$professional->phone}}</td>
-					<td class="table-active">
-						<a href="">
-							<button type="button" class="btn btn-success btn-sm">Exibir</button>
-						</a>
-					</td>
-					<td class="table-active">
-						<a href="">
-							<button type="button" class="btn btn-warning btn-sm">Editar</button>
-						</a>
-					</td>
-					<td class="table-active">	
-						<a href="">
-							<button type="button" class="btn btn-danger btn-sm">Apagar</button>
-						</a>
-					</td>
-				</tr>
-				@endforeach
-			</table>
+			<div scope="row">
+				<table class="table table-hover table-sm border border-dark">
+					<thead class="thead-dark">
+						<tr>
+							<th scope="col">Nome</td>
+							<th scope="col">Área de Atuação</td>
+							<th scope="col">Licença Profissional</td>
+							<th scope="col" class="collapse" id="cpf">CPF</td>
+							<th scope="col" class="collapse" id="phone">Celular</td>
+							<th scope="col-sm-1">
+								<a href="{{route('professional.create')}}">
+									<button class="btn btn-primary btn-sm">Novo</button>
+								</a>
+							</th>
+						</tr>
+					</thead>
+					@foreach($professionals as $professional)
+					<tbody>
+						<tr>
+							<td scope="col">{{$professional->name}}</td>
+							<td scope="col">{{$professional->prof}}</td>
+							<td scope="col">{{$professional->license}}</td>
+							<td scope="col" class="collapse" id="cpf">{{$professional->cpf}}</td>
+							<td scope="col" class="collapse" id="phone">{{$professional->phone}}</td>
+							<td class="d-flex flex-row">
+								<nav>
+									<a class="nav-link dropdown-toggle btn btn-success btn-sm" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"></a>
+									<div class="dropdown-menu">
+										<a class="dropdown-item text-success" href="#">Exibir</a>
+										<a class="dropdown-item text-warning" href="#">Editar</a>
+										<a class="dropdown-item text-danger" href="#">Deletar</a>
+									</div>
+								</nav>
+							</td>
+						</tr>
+					</tbody>
+					@endforeach
+				</table>
+			</div>
 		</div>
 	@include('down')
+	<script>
+		if (($(window).width()) > 500) {
+			$('.collapse').show();
+		}
+	</script>
 	</body>
 </html>

b299e9d1010df5e0b2e50f4347789d07e73c74df
jenvieiras <jennifervsouza@icloud.com>
2020-06-16 22:53:57 -0300

    new name in column and new column in table addresses

--- multi/app/Address.php
+++ multi/app/Address.php
@@ -9,7 +9,6 @@ class Address extends Model
     protected $table = 'addresses';
 
     protected $fillable = [
-        'patient_id', 'professional_id', 'zip', 'address', 'number', 'complement', 'neighborhood', 'city'
+        'patient_id', 'professional_id', 'zip', 'street', 'number', 'complement', 'neighborhood', 'city', 'state'
     ];
-
 }
